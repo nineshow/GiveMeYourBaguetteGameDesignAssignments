a18593bed5c4d33740e7c74edac029fc02de5af2
Wei Ng <wei-i2008@hotmail.com>
2026-06-19 14:23:00 +0800

    增加了加载或重启Scene的功能

---
+++ Assets/Script/LevelManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance;
+
+    private void Awake()
+    {
+        if(Instance==null)
+        {
+            Instance=this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadNextLevel(int nextSceneID)
+    {
+        //check if scene exist first
+        if(nextSceneID<0||nextSceneID>= UnityEngine.
+        SceneManagement.SceneManager.sceneCountInBuildSettings)
+        {
+            Debug.LogError("Invalid scene ID: "+nextSceneID);
+            return;
+        }
+        SceneManager.LoadScene(nextSceneID);
+    }
+
+    public void RestartLevel()
+    {
+        int currentLevelID=SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentLevelID);
+    }
+}
