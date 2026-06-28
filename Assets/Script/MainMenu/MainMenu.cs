8b46b01a0fb6fa1cc7f07a665d8499b807d12e3d
Wei Ng <wei-i2008@hotmail.com>
2026-06-28 15:41:56 +0800

    添加了主菜单

---
+++ Assets/Script/MainMenu/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // Load the first level (assuming it's at index 1)
+        Time.timeScale = 1f; // Ensure the game is not paused when starting
+        LevelManager.Instance.LoadLevel(1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting the game...");
+        Application.Quit();
+    }
+}
